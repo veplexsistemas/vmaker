cd84088a5021da4b1fa5d06e6dadc75b5daf4a48
Matheus da Silva Lutz <matheusdasilvalutz@gmail.com>
2019-03-05 12:11:06 -0300

    Ajustando VHtml

--- src/VHtml.php
+++ src/VHtml.php
@@ -5,23 +5,75 @@
   
   class VHtml
   {
+    /**
+     * @var string
+     */
     protected $layout;
     
-    protected $output = "";
+    /**
+     * @var string
+     */
+    protected $output;
     
-    public function __construct($layout)
+    /**
+     * Construct
+     * @param string $layout
+     */
+    public function __construct($layout = null)
     {
       $this->setLayout($layout);
     }
     
+    /**
+     * @param string $section
+     */
+    public function openSection($section)
+    {
+      $this->output .= "@section('$section')";
+    }
+    
+    /**
+     * Close section
+     */
+    public function closeSection()
+    {
+      $this->output .= "@endsection";
+    }
+    
+    /**
+     * @param string $layout
+     */
     public function setLayout($layout)
     {
       $this->layout = $layout;
+      $this->output .= "@extends('{$this->layout}')";
     }
     
+    /**
+     * @param string $content
+     */
+    public function addContent($content)
+    {
+      if (strlen(trim($content)))
+        $this->output .= $content;
+    }
+    
+    /**
+     * Add object to HTML
+     * @param object $object (Instance of VForm, VTable or VObject)
+     */
+    public function addObject($object)
+    {
+      if ($object instanceof VForm || $object instanceof VTable || $object instanceof VObject)
+        $this->output .= $object->make();
+    }
+    
+    /**
+     * Makes Html
+     * @return string
+     */
     public function make()
     {
-      $this->output .= "";
-      return StringView::make("@extends('layouts.app')")->render();
+      return StringView::make($this->output)->render();
     }
   }
