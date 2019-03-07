2399bd047311fc337bafcd6726a7a80e02fb2428
Matheus da Silva Lutz <matheusdasilvalutz@gmail.com>
2019-03-06 21:24:22 -0300

    Alterações Formatted Number

--- src/VInputFormattedNumber.php
+++ src/VInputFormattedNumber.php
@@ -11,6 +11,16 @@
   class VInputFormattedNumber extends VObject
   {
     /**
+     * @var int
+     */
+    protected $precision = 2;
+    
+    /**
+     * @var boolean
+     */
+    protected $allowNegative = false;
+    
+    /**
      * Constructor
      * @param string $id
      * @param mixed $defaultValue
@@ -32,8 +42,24 @@
       if ($this->useDiv)
         $this->output .= "</div>";
       
-      $this->output .= "<script>$('#{$this->id}').maskMoney(); </script>";
+      $this->output .= "<script>$('#{$this->id}').maskMoney({precision: {$this->precision}, allowNegative: {$this->allowNegative}, thousands: '.', decimal: ','}); </script>";
       
       return $this->output;
+    }
+    
+    /**
+     * @param int $precision
+     */
+    function setPrecision($precision)
+    {
+      $this->precision = $precision;
+    }
+    
+    /**
+     * @param boolean $allowNegative
+     */
+    function setAllowNegative($allowNegative)
+    {
+      $this->allowNegative = $allowNegative;
     }
   }
