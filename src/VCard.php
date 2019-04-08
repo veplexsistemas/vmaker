48a2ba0b87a719905a4f4a13d9e2c9694a6712d0
Matheus da Silva Lutz <matheusdasilvalutz@gmail.com>
2019-04-08 20:02:34 -0300

    Adicionando VCard

---
+++ src/VCard.php
@@ -0,0 +1,80 @@
+<?php
+  namespace VMaker;
+  
+  class VCard extends vPrimitiveObject
+  {
+    /**
+     * @var string
+     */
+    protected $headerClass = "card-header";
+    
+    /**
+     * @var string
+     */
+    protected $bodyClass = "card-body";
+    
+    /**
+     * @var string
+     */
+    protected $footerClass = "card-footer";
+    
+    
+    public function __construct()
+    {
+      $this->class = "card";
+    }
+    
+    /**
+     * @param string $content
+     */
+    public function addHeader($content)
+    {
+      $this->output .= "<div class=\"{$this->headerClass}\">{$content}</div>";
+    }
+    
+    /**
+     * @param string $content
+     */
+    public function addBody($content)
+    {
+      $this->output .= "<div class=\"{$this->bodyClass}\">{$content}</div>";
+    }
+    
+    /**
+     * @param string $content
+     */
+    public function addFooter($content)
+    {
+      $this->output .= "<div class=\"{$this->footerClass}\">{$content}</div>";
+    }
+    
+    public function make()
+    {
+      parent::make();
+      return $this->output;
+    }
+    
+    /**
+     * @param string $headerClass
+     */
+    public function setHeaderClass($headerClass)
+    {
+      $this->headerClass = $headerClass;
+    }
+    
+    /**
+     * @param string $bodyClass
+     */
+    public function setBodyClass($bodyClass)
+    {
+      $this->bodyClass = $bodyClass;
+    }
+
+    /**
+     * @param string $footerClass
+     */
+    public function setFooterClass($footerClass)
+    {
+      $this->footerClass = $footerClass;
+    }
+  }
